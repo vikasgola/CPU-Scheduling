db4cca632b3528eb74310e8fb468cc71c504da3c
AakarSharma <aakarsharmathegreat@gmail.com>
2019-02-14 12:46:42 +0530

    corrected mlfq

--- src/mlfq.h
+++ src/mlfq.h
@@ -16,45 +16,43 @@ void mlfq(vector<process> &work, vector<process> &process_completed,
     vector<int> V[3];
     int MAX_ROBIN;
     static int reset = 0;
-
-    for (int i = 0; i < work.size(); i++) {
-        V[work[i].custom_priority - 1].push_back(i);
-        count[work[i].custom_priority - 1]++;
-        if(reset >= 100){
-            work[i].custom_priority = 1;
+    if (!work.empty()) {
+        for (int i = 0; i < work.size(); i++) {
+            V[work[i].custom_priority - 1].push_back(i);
+            count[work[i].custom_priority - 1]++;
+            if (reset >= 100) {
+                work[i].custom_priority = 1;
+            }
         }
-    }
 
-    if(reset >= 100){
-        reset = 0;
-    }
+        if (reset >= 100) {
+            reset = 0;
+        }
 
-    if (count[0] != 0) {
-        current_custom_priority = 1;
-        MAX_ROBIN = 1;
-    } else if (count[1] != 0) {
-        current_custom_priority = 2;
-        MAX_ROBIN = 2;
-    } else if (count[2] != 0) {
-        current_custom_priority = 3;
-        MAX_ROBIN = 3;
-    }
-
-    if (previous_custom_priority != current_custom_priority) {
+        if (count[0] != 0) {
+            current_custom_priority = 1;
+            MAX_ROBIN = 1;
+        } else if (count[1] != 0) {
+            current_custom_priority = 2;
+            MAX_ROBIN = 2;
+        } else if (count[2] != 0) {
+            current_custom_priority = 3;
+            MAX_ROBIN = 3;
+        }
         round_robin_time = 0;
         index = 0;
-        if (current_custom_priority == 1) {
-            working_on = V[0][0];
-        } else if (current_custom_priority == 2) {
-            working_on = V[1][0];
-        } else if (current_custom_priority == 3) {
-            working_on = V[2][0];
+        if (previous_custom_priority != current_custom_priority) {
+            if (current_custom_priority == 1) {
+                working_on = V[0][0];
+            } else if (current_custom_priority == 2) {
+                working_on = V[1][0];
+            } else if (current_custom_priority == 3) {
+                working_on = V[2][0];
+            }
+        } else {
+            working_on = V[current_custom_priority - 1][index];
         }
-    } else {
-        working_on = V[current_custom_priority - 1][index];
-    }
 
-    if (!work.empty()) {
         work[working_on].mark = true;
         work[working_on].jobs[0].burst_time--;
         table(process_CPU, process_input, process_output, process_completed,
